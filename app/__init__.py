f8f63120ea7f9c10d5b8f9304af6323da6bdf63b
kevaf <kevinkipyegon20@gmail.com>
2021-09-17 16:37:04 +0300

    added classes to model

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,19 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_mail import Mail
+from flask_simplemde import SimpleMDE 
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+photos = UploadSet('photos', IMAGES)
+mail = Mail()
+simple = SimpleMDE()
 
 def create_app(config_name):
     
@@ -16,9 +26,16 @@ def create_app(config_name):
     #initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    simple.init_app(app)
 
     #registering blueprints
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+
+    # configure UploadSet
+    configure_uploads(app,photos)
 
     return app
--- app/models.py
+++ app/models.py
@@ -1,1 +1,100 @@
+from . import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+from . import login_manager
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(1000))
+    profile_pic_path = db.Column(db.String)
+    pass_secure = db.Column(db.String(255))
+    pitches = db.relationship('Pitch',backref = 'user',lazy = "dynamic")
+    reviews = db.relationship('Review',backref = 'user',lazy = "dynamic")
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self,password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+
+class Pitch(db.Model):
+    __tablename__ = "pitches"
+
+    id = db.Column(db.Integer,primary_key = True)
+    p_title = db.Column(db.String)
+    p_body = db.Column(db.String(1000))
+    category = db.Column(db.String)
+    posted_date = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    upvote = db.Column(db.Integer)
+    downvote = db.Column(db.Integer)
+    reviews = db.relationship('Review',backref =  'pitch_id',lazy = "dynamic")
+
+
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_pitches(cls,category):
+        pitches = Pitch.query.filter_by(category=category).all()
+        return pitches
+
+    @classmethod
+    def get_pitch(cls,id):
+        pitch = Pitch.query.filter_by(id=id).first()
+
+        return pitch
+    @classmethod
+    def count_pitches(cls,uname):
+
+        user = User.query.filter_by(username=uname).first()
+        pitches = Pitch.query.filter_by(user_id=user.id).all()
+
+
+        pitches_count = 0
+        for pitch in pitches:
+            pitches_count += 1
+
+        return pitches_count
+
+class Review(db.Model):
+
+    __tablename__ = 'reviews'
+    id = db.Column(db.Integer,primary_key = True)
+    review = db.Column(db.String(500))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    pitch = db.Column(db.Integer,db.ForeignKey("pitches.id"))
+
+  
+
+    def save_review(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_reviews(cls,pitch):
+        reviews = Review.query.filter_by(pitch_id=pitch).all()
+        return reviews
+
 
