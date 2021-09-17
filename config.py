35df6a56372eadf3625ec20fb33fc1a110d9ded3
kevaf <kevinkipyegon20@gmail.com>
2021-09-17 11:47:15 +0300

    innitialized db

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:kev2214@localhost/exhibitpitch'
 
 class ProdConfig(Config):
     '''
