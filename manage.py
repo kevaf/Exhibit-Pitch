69b3042f88aa1cff11ae0dbbbd8c1eb7e339543c
kevaf <kevinkipyegon20@gmail.com>
2021-09-18 15:27:31 +0300

    fixed log in bug

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@ from app import create_app, db
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 from app.models import User
+
 #creating app instance
 app = create_app('development')
 
---
+++ start.sh
@@ -0,0 +1,3 @@
+export SECRET_KEY ='balliswithkevin'
+
+python3.8 manage.py server
